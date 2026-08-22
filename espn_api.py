e53dd9ad1ad0667c72bdf69ab75e4f90f064e575
John <jfchagaris@gmail.com>
2026-08-22 17:37:32 -0400

    guard against empty statistics

--- espn_api.py
+++ espn_api.py
@@ -35,7 +35,10 @@ def player_stats(player, league=None, sport=None):
             for c in categories:
                 if c["name"] == "advanced-batting":
                     labels = c["labels"]
-                    season_stats = c["statistics"][-1]["stats"]
+                    season_stats = c.get("statistics", [])
+                    if not season_stats:
+                        continue
+                    season_stats = season_stats[-1]["stats"]
                     stat_map = dict(zip(labels, season_stats))
                     war = stat_map.get("WAR", "n/a")
                     bb_pa = stat_map.get("BB/PA", "n/a")
@@ -44,7 +47,10 @@ def player_stats(player, league=None, sport=None):
                     stats_list.append(f"WAR: {war} / RC: {rc} / BB/PA: {bb_pa} / BB/K: {bb_k}")
                 elif c["name"] == "expanded-pitching":
                     labels = c["labels"]
-                    season_stats = c["statistics"][-1]["stats"]
+                    season_stats = c.get("statistics")
+                    if not season_stats:
+                        continue
+                    season_stats = season_stats[-1]["stats"]
                     stat_map = dict(zip(labels, season_stats))
                     k_9 = stat_map.get("K/9", "n/a")
                     gb = stat_map.get("GB", "n/a")
